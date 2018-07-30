f61a5853187983048e89b795f068668b01d06ea4
VolodymyrMarchenko <nokialeader@gmail.com>
2018-07-30 14:47:28 +0300

    galllery

--- src/js/_fancy.js
+++ src/js/_fancy.js
@@ -1,7 +1,13 @@
 import '../../node_modules/fancybox/dist/js/jquery.fancybox.js';
+import '../../node_modules/fancybox/dist/helpers/js/jquery.fancybox-buttons.js';
 $(document).ready(function() {
-  $('.fancybox').fancybox({
-    openEffect	: 'none',
-    closeEffect	: 'none'
+  $('.fancybox-button').fancybox({
+    prevEffect		: 'none',
+    nextEffect		: 'none',
+    closeBtn		: 'none',
+    helpers		: {
+      title	:  'none' ,
+      buttons	: {}
+    }
   });
 });
